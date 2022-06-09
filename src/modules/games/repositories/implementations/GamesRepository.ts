fff2f1173842911f503774041ba64f148ed63cf3
Elcio Freitas <elcio.cf@gmail.com>
2022-06-08 23:44:46 -0300

    solved

--- src/modules/games/repositories/implementations/GamesRepository.ts
+++ src/modules/games/repositories/implementations/GamesRepository.ts
@@ -7,24 +7,37 @@ import { IGamesRepository } from '../IGamesRepository';
 
 export class GamesRepository implements IGamesRepository {
   private repository: Repository<Game>;
+  private userRepository: Repository<User>;
 
   constructor() {
     this.repository = getRepository(Game);
+    this.userRepository = getRepository(User);
   }
 
   async findByTitleContaining(param: string): Promise<Game[]> {
-    return this.repository
-      .createQueryBuilder()
+    return await this.repository
+      .createQueryBuilder('games')
+      .where('games.title ilike :pesq',{ pesq : `%${param}%`}).getMany()
       // Complete usando query builder
   }
 
   async countAllGames(): Promise<[{ count: string }]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query('select count(-1) from games'); // Complete usando raw query
   }
 
   async findUsersByGameId(id: string): Promise<User[]> {
-    return this.repository
-      .createQueryBuilder()
+    return this.userRepository
+      .createQueryBuilder("users")
+      .innerJoin("users.games", "game")
+      .where("game.id = :game", {game: id})
+      .getMany()
+
+      
+      
+      
+      
+      
+
       // Complete usando query builder
   }
 }
